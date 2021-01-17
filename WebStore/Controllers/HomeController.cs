f7ca7ec430589e795e9c7162d091198a026ea9b3
Kristina <8_klukovka_8@inbox.ru>
2021-01-17 18:26:43 +0300

    Представления

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers 
+{
+    public class HomeController : Controller
+    {
+        //контроллер нужен для того, чтобы обработать входящее подключение
+        // на каждый контроллер должно быть свое представление Views
+
+        public IActionResult Index() => View("SecondView");        
+
+        public IActionResult SecondAction()
+        {
+            return Content("Second controller action");
+        }
+    }
+}
