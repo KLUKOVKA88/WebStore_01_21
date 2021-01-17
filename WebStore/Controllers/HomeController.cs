3cb43b3b3f1bda860f208e57132cd9446c4b8aa8
Kristina <8_klukovka_8@inbox.ru>
2021-01-17 18:53:04 +0300

    Визуализация списка сотрудников

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebStore.Models;
 
 namespace WebStore.Controllers 
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 37 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 25 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 27 },
+        };
+
+
         //контроллер нужен для того, чтобы обработать входящее подключение
         // на каждый контроллер должно быть свое представление Views
 
@@ -13,5 +23,11 @@ namespace WebStore.Controllers
         {
             return Content("Second controller action");
         }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
+        }
+
     }
 }
