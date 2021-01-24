c0eeb4138d3709aa752a2b387a51cbd94e64d981
Kristina <8_klukovka_8@inbox.ru>
2021-01-24 17:22:45 +0300

    Промежуточное ПО

---
+++ WebStore/Infrastructure/Middleware/TestMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WebStore.Infrastructure.Middleware
+{
+    public class TestMiddleware
+    {
+        private readonly RequestDelegate _Next;
+        public TestMiddleware(RequestDelegate Next) =>  _Next = Next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Действие до след узла в конвейере
+            //context.Response.
+
+            var next = _Next(context);
+
+            //Действие во время того, как оставшаяся часть конвейера что-то там делает с контекстом
+
+            await next; //Точка синхронизации
+
+            //Действие по завершении работы оставшейся части конвейера
+
+        }
+
+    }
+}
