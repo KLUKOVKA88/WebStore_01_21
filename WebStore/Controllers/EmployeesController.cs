fd960055d5f583694b47ef863e9cec4b50a7e287
Kristina <8_klukovka_8@inbox.ru>
2021-01-26 12:47:31 +0300

    Удаление сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -28,6 +28,8 @@ namespace WebStore.Controllers
             return NotFound();               
         }
 
+      
+
         #region Edit
         public IActionResult Edit(int id)
         {
@@ -65,6 +67,33 @@ namespace WebStore.Controllers
 
             _EmployeesData.Update(employee);
 
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Delete
+        public IActionResult Delete(int id)
+        {
+            if (id <= 0) return BadRequest();
+
+            var employee = _EmployeesData.Get(id);
+
+            if (employee is null)
+                return NotFound();
+
+            return View(new EmployeeViewModel
+            {
+                Id = employee.Id,
+                LastName = employee.LastName,
+                Name = employee.FirstName,
+                MiddleName = employee.Patronymic,
+                Age = employee.Age
+            });
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _EmployeesData.Delete(id);
             return RedirectToAction("Index");
         }
         #endregion
