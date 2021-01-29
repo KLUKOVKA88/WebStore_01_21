7e3af5d1dc92247a49886c5e61db7e1d8058ef97
Kristina <8_klukovka_8@inbox.ru>
2021-01-29 10:12:49 +0300

    Описание сущностей Бренда и Секции

---
+++ WebStore.Domain/Entities/Brand.cs
@@ -0,0 +1,16 @@
+﻿using WebStore.Domain.Entities.Base;
+using WebStore.Domain.Entities.Base.Interfaces;
+
+namespace WebStore.Domain.Entities
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+    public class Section : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+
+        public int? ParentId { get; set; }
+    }
+}
