571160ee4ea3a10e7b03508c1d3f7d4c11e5f1a7
Kristina <8_klukovka_8@inbox.ru>
2021-01-29 14:01:07 +0300

    Контроллер каталога товаров

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Infrastructure.Interfaces;
 
@@ -9,7 +11,19 @@ namespace WebStore.Infrastructure.Services
     {
         public IEnumerable<Section> GetSections() => TestData.Sections;
         
-        public IEnumerable<Brand> GetBrands() => TestData.Brands;       
-       
+        public IEnumerable<Brand> GetBrands() => TestData.Brands;      
+        
+        public IEnumerable<Product> GetProducts(ProductFilter Filter)
+        {
+            var query = TestData.Products;
+
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(product => product.SectionId == section_id);
+
+            if (Filter?.BrandId is { } brand_id)
+                query = query.Where(product => product.BrandId == brand_id);
+
+            return query;
+        }
     }
 }
