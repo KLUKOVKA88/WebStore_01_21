9f2e17f3013137647ccf4652b803367139d1715e
Kristina <8_klukovka_8@inbox.ru>
2021-02-05 10:36:02 +0300

    Регистрация пользователя - контроллер Account

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 using WebStore.Domain.Entities.Identity;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -8,14 +10,37 @@ namespace WebStore.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<User> _UserManager;
-        private readonly RoleManager<Role> _RoleManager;
+        private readonly SignInManager<User> _SignInManager;
 
-        public AccountController(UserManager<User> UserManager, RoleManager<Role> RoleManager)
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
         {
             _UserManager = UserManager;
-            _RoleManager = RoleManager;
+            _SignInManager = SignInManager;
         }
 
+        public IActionResult Register() => View(new RegisterUserViewModel());
 
+        [HttpPost, ValidateAntiForgeryToken/*, ActionName ("Register")*/]
+        public async Task<IActionResult> Register/*Async*/(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var registration_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (registration_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+            return View(Model);
+        }
     }
 }
